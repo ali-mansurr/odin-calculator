df84687caa949518928c6a8d263af6370879c5f4
ali-mansurr <197986189+ali-mansurr@users.noreply.github.com>
2025-08-10 15:35:49 +0330

    Define four basic operation functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function Add(a, b) {
+  return a + b;
+}
+
+function Subtract(a, b) {
+  return a - b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
